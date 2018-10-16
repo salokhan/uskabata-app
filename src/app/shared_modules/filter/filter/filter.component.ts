eb52e83e2d95383684984111124065680aa47d9f
Salman Khan <im.solomon@outlook.com>
2018-10-16 18:20:57 +0500

    BehaviorSubject input with subscribe

--- src/app/shared_modules/filter/filter/filter.component.ts
+++ src/app/shared_modules/filter/filter/filter.component.ts
@@ -1,24 +1,17 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { SelectItemGroup } from 'primeng/api';
 import { ICity } from '../../city';
 import { ICategory } from '../../category';
 import { IExperty } from '../../experty';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnChanges {
-
-  groupedCities: SelectItemGroup[] = [];
-  groupedCategories: SelectItemGroup[] = [];
-  groupedExperties: SelectItemGroup[] = [];
-
-  @Input() cities: ICity[];
-  @Input() categories: ICategory[];
-  experties: IExperty[];
+export class FilterComponent implements OnInit {
 
   filterForm = new FormGroup({
     selectedCity: new FormControl(),
@@ -26,38 +19,70 @@ export class FilterComponent implements OnChanges {
     selectedExperty: new FormControl()
   });
 
-  constructor() {
+  groupedCities: SelectItemGroup[] = [];
+  groupedCategories: SelectItemGroup[] = [];
+  groupedExperties: SelectItemGroup[] = [];
+
+  // initialize a private variable cities, it's a BehaviorSubject
+  private _cities = new BehaviorSubject<ICity[]>([]);
+  // change data to use getter and setter
+  @Input()
+  set cities(value) {
+    // set the latest value for _cities BehaviorSubject
+    this._cities.next(value);
   }
+  get cities() {
+    // get the latest value from _cities BehaviorSubject
+    return this._cities.getValue();
+  }
+
+  private _categories = new BehaviorSubject<ICategory[]>([]);
+  @Input()
+  set categories(value) {
+    this._categories.next(value);
+  }
+  get categories() {
+    return this._categories.getValue();
+  }
+
+  experties: IExperty[];
+
+  ngOnInit() {
+    // now we can subscribe to it, whenever input changes, we will run our grouping logic
+    this._cities.subscribe(data => {
+      if (this.cities) {
+        const grouped = this.groupBy(this.cities, city => city.type);
+        grouped.forEach(element => {
+          this.groupedCities.push({ 'label': element[0].type, items: [] });
+          element.forEach(city => {
+            const selectedGroup = this.groupedCities.find(x => x.label === city.type);
+            if (selectedGroup) {
+              selectedGroup.items.push({ label: city.name, value: city.name });
+            }
+          });
+        });
+      }
+    });
+
+    this._categories.subscribe(data => {
+      if (this.categories) {
+        const grouped = this.groupBy(this.categories, category => category.type);
+        grouped.forEach(element => {
+          this.groupedCategories.push({ 'label': element[0].type, items: [] });
+          element.forEach(category => {
+            const selectedGroup = this.groupedCategories.find(x => x.label === category.type);
+            if (selectedGroup) {
+              selectedGroup.items.push({ label: category.name, value: category.name });
+            }
+          });
+        });
+      }
+    });
+  }
+
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.filterForm.value);
-  }
-  ngOnChanges() {
-    if (this.cities) {
-      const grouped = this.groupBy(this.cities, city => city.type);
-      grouped.forEach(element => {
-        this.groupedCities.push({ 'label': element[0].type, items: [] });
-        element.forEach(city => {
-          const selectedGroup = this.groupedCities.filter(x => x.label === city.type);
-          if (selectedGroup && selectedGroup.length > 0) {
-            selectedGroup[0].items.push({ label: city.name, value: city.name });
-          }
-        });
-      });
-    }
-
-    if (this.categories) {
-      const grouped = this.groupBy(this.categories, category => category.type);
-      grouped.forEach(element => {
-        this.groupedCategories.push({ 'label': element[0].type, items: [] });
-        element.forEach(category => {
-          const selectedGroup = this.groupedCategories.filter(x => x.label === category.type);
-          if (selectedGroup && selectedGroup.length > 0) {
-            selectedGroup[0].items.push({ label: category.name, value: category.name });
-          }
-        });
-      });
-    }
   }
 
   onCategorySelection(): void {
@@ -70,9 +95,9 @@ export class FilterComponent implements OnChanges {
         grouped.forEach(element => {
           this.groupedExperties.push({ 'label': element[0].type, items: [] });
           element.forEach(experty => {
-            const selectedGroup = this.groupedExperties.filter(x => x.label === experty.type);
-            if (selectedGroup && selectedGroup.length > 0) {
-              selectedGroup[0].items.push({ label: experty.name, value: experty.name });
+            const selectedGroup = this.groupedExperties.find(x => x.label === experty.type);
+            if (selectedGroup) {
+              selectedGroup.items.push({ label: experty.name, value: experty.name });
             }
           });
         });
