bc8c0798ef3a1177eea502e8225c3ef1f7b9107f
Salman Khan <im.solomon@outlook.com>
2018-10-22 19:23:36 +0500

    user module and user-profile component is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule, Component } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { HttpModule } from '@angular/http';
-
-
 
 import { AppComponent } from './app.component';
 import { TopBarModule } from './core_modules/top-bar/top-bar.module';
 import { AppRoutingModule } from './app-routing.module';
-
-
-
 
 
 @NgModule({
