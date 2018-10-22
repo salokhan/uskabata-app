176e8f6d0eb62f3c425cf3fa95530055ab8779ed
Salman Khan <im.solomon@outlook.com>
2018-10-23 00:21:25 +0500

    profile changes

--- src/app/feature_modules/user/user-profile/user-profile.component.ts
+++ src/app/feature_modules/user/user-profile/user-profile.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
 
   constructor() { }
-  name = new FormControl('');
+
+  userProfileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormControl(''),
+    address2: new FormControl(''),
+    state : new FormControl(''),
+    city: new FormControl('')
+  });
+
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.userProfileForm.value);
   }
 
 }
--- src/app/feature_modules/user/user.module.ts
+++ src/app/feature_modules/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [UserProfileComponent]
 })
