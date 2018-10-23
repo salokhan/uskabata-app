5d6f2968ae033d7670badddd29e5eec9db023d6d
Salman Khan <im.solomon@outlook.com>
2018-10-23 19:39:44 +0500

    validation and form prepared

--- src/app/feature_modules/user/user-profile/user-profile.component.ts
+++ src/app/feature_modules/user/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,19 +8,69 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  userProfileForm: FormGroup;
+  mobileContacts: FormArray;
+  landLineContacts: FormArray;
 
-  userProfileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    address: new FormControl(''),
-    address2: new FormControl(''),
-    state : new FormControl(''),
-    city: new FormControl('')
-  });
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.userProfileForm = this._formBuilder.group({
+      firstName: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      lastName: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      address: new FormGroup({
+        addressLine: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+        city: new FormControl(''),
+        state: new FormControl('')
+      }),
+      mobileContacts: this._formBuilder.array([this.createMobileContact()]),
+      landLineContacts: this._formBuilder.array([this.createLandLineContact()])
+    });
+  }
+
+  createMobileContact(): FormGroup {
+    // if the first control is creating
+    if (!this.userProfileForm) {
+      return this._formBuilder.group({
+        mobileNumber: ['', [Validators.required, Validators.pattern(/^\+[1-9]{1}[0-9]{3,14}$/)]],
+      });
+    } else {
+      return this._formBuilder.group({
+        mobileNumber: ['', [Validators.pattern(/^\+[1-9]{1}[0-9]{3,14}$/)]],
+      });
+    }
+  }
+
+  addMobileContact(): void {
+    this.mobileContacts = this.userProfileForm.get('mobileContacts') as FormArray;
+    this.mobileContacts.push(this.createMobileContact());
+  }
+
+  removeMobileContact(index: number): void {
+    if (index !== 0) {
+      this.mobileContacts = this.userProfileForm.get('mobileContacts') as FormArray;
+      this.mobileContacts.removeAt(index);
+    }
+  }
+
+  createLandLineContact(): FormGroup {
+    return this._formBuilder.group({
+      landLineNumber: ['', [Validators.pattern(/^\+[1-9]{1}[0-9]{3,14}$/)]],
+    });
+  }
+
+  addLandLineContact(): void {
+    this.landLineContacts = this.userProfileForm.get('landLineContacts') as FormArray;
+    this.landLineContacts.push(this.createLandLineContact());
+  }
+
+  removeLandLineContact(index: number): void {
+    if (index !== 0) {
+      this.landLineContacts = this.userProfileForm.get('landLineContacts') as FormArray;
+      this.landLineContacts.removeAt(index);
+    }
   }
 
   onSubmit() {
