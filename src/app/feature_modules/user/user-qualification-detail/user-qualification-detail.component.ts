7c8b8d5d70b93116d12541fc8252067a5fec7d66
Salman Khan <im.solomon@outlook.com>
2018-10-26 19:22:49 +0500

    tabs renamed to detail

--- src/app/feature_modules/user/user-qualification-detail/user-qualification-detail.component.ts
+++ src/app/feature_modules/user/user-qualification-detail/user-qualification-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-qualification-detail',
   templateUrl: './user-qualification-detail.component.html',
-  styleUrls: ['./user-qualification-detail.component.css']
+  styleUrls: ['./user-qualification-detail.component.scss']
 })
 export class UserQualificationDetailComponent implements OnInit {
 
